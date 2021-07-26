d07b4ea2141e5a76ac20641e5401e4de5e27ed14
shmool <shmuelaj@gmail.com>
2021-07-26 13:10:53 +0300

    add test login component and auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginTestComponent } from './login-test/login-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginTestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login-test/login-test.component.ts
@@ -0,0 +1,19 @@
+import { AuthService } from './../auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-login-test',
+  templateUrl: './login-test.component.html',
+  styleUrls: ['./login-test.component.scss']
+})
+export class LoginTestComponent implements OnInit {
+  user$!: Observable<any>;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.user$ = this.authService.getUser$();
+  }
+
+}
