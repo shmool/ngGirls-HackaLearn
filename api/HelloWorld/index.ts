02bfa815524ef7c0ca251c4709b245ebc657c8a0
shmool <shmuelaj@gmail.com>
2021-07-27 12:54:50 +0300

    add env variable

--- api/HelloWorld/index.ts
+++ api/HelloWorld/index.ts
@@ -2,7 +2,8 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
+    const secretName = process.env.SECRET_NAME;
+    const name = (req.query.name || (req.body && req.body.name) || secretName);
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
